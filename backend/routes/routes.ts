d3bf37abba2e207faab0fcf915bd729d6425e7eb
Sami <143815018+samuelgt3@users.noreply.github.com>
2026-06-08 20:14:23 -0400

    added prompts.json, worked on routes

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,42 +1,64 @@
-import Anthropic from "@anthropic-ai/sdk";
+import Anthropic from "@anthropic-ai/sdk"
+import { get } from "node:https";
+import { run } from "node:test";
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
 const app = express();
 const port = 3000;
+let prompts = require("prompts.json")
 
+
+prompts["Output"]
 
 const client = new Anthropic({
-  apiKey: process.env.CLAUDE_KEY// This is the default and can be omitted
+  apiKey: process.env.CLAUDE_KEY
 });
 
-const message = await client.messages.create({
-  max_tokens: 1024,
-  messages: [{ role: "user", content: "Hello, Claude" }],
-  model: "claude-opus-4-8"
-});
-
-console.log(message.content);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('The server is running. Please deploy the react app to interact with it');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+async function runcode(msg=JSON){
 
-app.post('/api/runcode', (msg=JSON) => {
-    return 0
-})
-app.post('/api/translate', (msg=JSON) => {
+    return }
+app.post('/api/runcode', runcode())
+app.post('/api/translate', async (msg=JSON) => {
+  if (!msg){
+    return {
+      "error": "error"
+    }
+  }
+  const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: "user", content: prompts["Translate"] + prompts["Output"] + msg}],
+  model: "claude-opus-4-8"
+});
   return 0
 });
 
-app.post('/api/retry', (msg=JSON, wrngmsg=JSON, errmsg=JSON) => {
-    return 0
+app.post('/api/improve', async (msg=JSON, wrngmsg=JSON, errmsg=JSON) => {
+  const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: "user", content: prompts["Improve"] + prompts["Output"] + msg}],
+  model: "claude-opus-4-8"
+});
+return message
 })
 
-export {}
+app.post('/api/correct', async (msg=JSON, wrngmsg=JSON, errmsg=JSON) => {
+  const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: "user", content: prompts["CorrectError"] + prompts["Output"] + msg}],
+  model: "claude-opus-4-8"
+});
+return message
+})
+
+export {app};
 
 
 
